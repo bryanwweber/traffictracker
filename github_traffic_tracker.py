53c8aa64123fe0d94709d7b721baa8dc258f8c7e
arghdos <arghdos@gmail.com>
2017-06-12 16:16:23 -0400

    flake8

--- github_traffic_tracker.py
+++ github_traffic_tracker.py
@@ -10,22 +10,25 @@ import pycurl
 from io import BytesIO
 from repo import repo_data
 
+
 def main():
-    #get credentials
+    # get credentials
     with open('access.cred', 'r') as file:
         cred = file.readlines()[0].strip()
 
-    #create command
-    curl_header = ['Authorization: token {0}'.format(cred), 'Accept: application/vnd.github.spiderman-preview']
+    # create command
+    curl_header = ['Authorization: token {0}'.format(
+        cred), 'Accept: application/vnd.github.spiderman-preview']
     curl_command = 'curl -i -H "" -H ""'
-    curl_url = 'https://api.github.com/repos/{0}/{1}/traffic/{{}}'.format(repo_data['user'], repo_data['repo'])
+    curl_url = 'https://api.github.com/repos/{0}/{1}/traffic/{{}}'.format(
+        repo_data['user'], repo_data['repo'])
     curl_command = curl_command.format(cred)
 
-    #create / open DB
+    # create / open DB
     db = sqlite3.connect('traffic.db', detect_types=sqlite3.PARSE_DECLTYPES)
     c = db.cursor()
 
-    #create schema
+    # create schema
     try:
         c.execute('''CREATE TABLE traffic
                     (time timestamp UNIQUE, views INTEGER, unique_views INTEGER)
@@ -43,11 +46,12 @@ def main():
             raise e
 
     tzinfo = datetime.now().tzinfo
+
     def __parse_row(row):
         try:
             date = dateutil.parser.parse(row['timestamp'], tzinfo)
         except:
-            #try the old style timestamp
+            # try the old style timestamp
             date = datetime.fromtimestamp(row['timestamp'] / 1000., tzinfo)
         count = int(row['count'])
         unique = int(row['uniques'])
@@ -57,7 +61,7 @@ def main():
     while True:
         try:
             view_data = BytesIO()
-            #create curl objects
+            # create curl objects
             py_views = pycurl.Curl()
             py_views.setopt(pycurl.URL, curl_url.format('views'))
             py_views.setopt(pycurl.HTTPHEADER, curl_header)
@@ -71,39 +75,39 @@ def main():
             #py_clones.setopt(pycurl.HEADER, 1)
             py_clones.setopt(pycurl.WRITEFUNCTION, clone_data.write)
 
-            #query view api
+            # query view api
             py_views.perform()
             views = json.loads(view_data.getvalue())
 
-            #parse into rows for DB
+            # parse into rows for DB
             view_rows = [__parse_row(row) for row in views['views']]
             c.executemany('INSERT OR REPLACE INTO traffic VALUES (?, ?, ?)',
-                        view_rows)
+                          view_rows)
 
-            #query clone api
+            # query clone api
             py_clones.perform()
             clones = json.loads(clone_data.getvalue())
 
-            #parse into rows for DB
+            # parse into rows for DB
             clone_rows = [__parse_row(row) for row in clones['clones']]
             c.executemany('INSERT OR REPLACE INTO clones VALUES (?, ?, ?)',
-                            clone_rows)
+                          clone_rows)
             db.commit()
 
             err_count = 0
 
-            #update every hour
+            # update every hour
             time.sleep(60 * 60)
 
         except KeyError as e:
-            raise Exception('Repo {}/{} not found!'.format(repo_data['user'], repo_data['repo']))
+            raise Exception(
+                'Repo {}/{} not found!'.format(repo_data['user'], repo_data['repo']))
 
         except Exception as e:
             err_count += 1
             time.sleep(5 * 60)
             if err_count == 10:
                 raise e
-
 
 
 if __name__ == '__main__':
