a73ab7f165fc6a3917d5392a0e1ae49e327f84e4
arghdos <arghdos@gmail.com>
2017-09-08 10:01:55 -0400

    add plotter to repo

---
+++ plotter.py
@@ -0,0 +1,54 @@
+import sqlite3
+from datetime import datetime, date
+import dateutil.parser
+import matplotlib.pyplot as plt
+import numpy as np
+import pytz
+
+
+utc = pytz.UTC
+tzinfo = datetime.now().tzinfo
+
+
+def timestamp_parser(val):
+    return dateutil.parser.parse(val, tzinfo).replace(tzinfo=utc)
+
+
+# create / open DB
+sqlite3.register_adapter(datetime, timestamp_parser)
+sqlite3.register_adapter(date, timestamp_parser)
+db = sqlite3.connect('traffic.db')
+c = db.cursor()
+
+# get clone data
+c.execute('SELECT time, unique_count from CLONES')
+
+dates = {}
+for row in c:
+    date = timestamp_parser(row[0])
+    dates[date] = row[1]
+
+# get sorted
+dates, vals = zip(*sorted(dates.iteritems(), key=lambda x: x[0]))
+vals = np.cumsum(vals)
+plt.plot(dates, vals)
+plt.ylabel('Total pyJac Clones')
+plt.xlabel('Date')
+plt.show()
+
+
+# get clone data
+c.execute('SELECT time, unique_views from TRAFFIC')
+
+dates = {}
+for row in c:
+    date = timestamp_parser(row[0])
+    dates[date] = row[1]
+
+# get sorted
+dates, vals = zip(*sorted(dates.iteritems(), key=lambda x: x[0]))
+vals = np.cumsum(vals)
+plt.plot(dates, vals)
+plt.ylabel('Total pyJac Views')
+plt.xlabel('Date')
+plt.show()
